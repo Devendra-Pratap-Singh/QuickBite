30d7506bfb7b7119010c2861020da1ac0e72dade
Devendra Pratap Singh <dpratapsingh42@gmail.com>
2025-09-14 20:44:53 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
 export const connectDB = async() => {
-    await mongoose.connect('mongodb+srv://Devendra:MUMMYandPAPA1@cluster0.ft6hctn.mongodb.net/food-delivery').then(()=>console.log("DB Connected"));
+    await mongoose.connect('mongodb+srv://Devendra:9871@cluster0.ft6hctn.mongodb.net/food-delivery').then(()=>console.log("DB Connected"));
 
 }
