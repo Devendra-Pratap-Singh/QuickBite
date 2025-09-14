80adaa9616e436b46c243d3744b3cb99f01a2476
Devendra Pratap Singh <dpratapsingh42@gmail.com>
2025-09-14 21:54:52 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ app.get("/",(req,res)=>{
     res.send("API Working")
 })
 
-app.listen(port, () => {
-    console.log(`✅ Server started on port ${port}`)
+app.listen(port,()=>{
+    console.log(`Server Started on http://localhost:${port}`)
 })
 
