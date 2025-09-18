aa4141d4c260f58bfb513c92016a0c673d72cd5e
Devendra Pratap Singh <dpratapsingh42@gmail.com>
2025-09-18 19:44:03 +0530

    Update PlaceOrder.jsx

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -2,6 +2,8 @@ import React, { useContext, useEffect, useState } from 'react'
 import './PlaceOrder.css'
 import axios from 'axios'
 import { StoreContext } from '../../context/StoreContext'
+import { useNavigate } from 'react-router-dom';
+
 
 const PlaceOrder = () => {
 
